7d4ef02e12bd2893c7af97db26111573d7a3e554
Juan Guecaimburu <guecaimburu.j@gmail.com>
2023-04-16 19:17:06 +0200

    Improvement: Adding view_component dependency and refactoring vertical navbar in views

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,8 @@ require "rspec/rails"
 
 require "sidekiq/testing"
 
+require "view_component/test_helpers"
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -66,4 +68,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include ViewComponent::TestHelpers, type: :component
+  config.include ViewComponent::SystemTestHelpers, type: :component
+  config.include Capybara::RSpecMatchers, type: :component
+  config.include Rails.application.routes.url_helpers, type: :component
 end
